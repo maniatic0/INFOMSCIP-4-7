e0a5a50ad1d990ab2311363a316f113386c0ad22
Christian Oliveros <christianol_01@hotmail.com>
2020-10-09 22:15:51 -0400

    First Question

--- R/main.r
+++ R/main.r
@@ -1,22 +1,25 @@
+
+library(class);
+
 generate_points <- function(n, min_size, max_size) 
 {
   X = runif(n, min=min_size, max=max_size);
   Y = runif(n, min=min_size, max=max_size);
   Color = replicate(length(X), 0);
   return (cbind(X, Y, Color))
-}
+};
 
 create_triangle <- function (p1, p2, p3)
 {
   return (rbind(p1, p2, p3));
-}
+};
 
 triangle_half_plane_sign <- function(p1, p2, p3)
 {
   # From https://stackoverflow.com/a/2049593
   # Basically a 2D outer product
   return ((p1[1] - p3[1]) * (p2[2] - p3[2]) - (p2[1] - p3[1]) * (p1[2] - p3[2]));
-}
+};
 
 is_inside_triangle <- function(point, triangle)
 {
@@ -34,21 +37,38 @@ is_inside_triangle <- function(point, triangle)
   has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
   
   return (!(has_neg && has_pos));
-}
+};
 
 classify_triangle <- function(points, triangle)
 {
-  p_len = dim(points)[1];
-  points[1:p_len, 3] = apply(points, c(1), is_inside_triangle, triangle);
+  points[, 3] = apply(points, c(1), is_inside_triangle, triangle);
+  return (points);
+};
+
+generate_outliers <- function(points, f)
+{
+  probable_flip <- function(point, f)
+  {
+    to_flip = runif(1) < f;
+    if(to_flip)
+    {
+      return(!point[3]);
+    }
+    else
+    {
+      return(point[3]); 
+    }
+  };
+  points[, 3] = apply(points, c(1), probable_flip, f);
   return (points);
 }
 
-draw_points <- function(points, triangle, min_size, max_size)
+draw_points <- function(points, triangle, min_size, max_size, f)
 {
   p_len = dim(points)[1];
   plot(
-    points[1:p_len, 1:2], 
-    col=ifelse(points[1:p_len, 3],"blue","red"), 
+    points[, 1:2], 
+    col=ifelse(points[, 3],"blue","red"), 
     xlim=c(min_size, max_size), 
     ylim=c(min_size,max_size),
     pch=16
@@ -60,9 +80,9 @@ draw_points <- function(points, triangle, min_size, max_size)
   segments(v1[1], v1[2], v2[1], v2[2], col="black", lwd=2);
   segments(v2[1], v2[2], v3[1], v3[2], col="black", lwd=2);
   segments(v3[1], v3[2], v1[1], v1[2], col="black", lwd=2);
-  points(v1[1], v1[2], col="green", lwd=2, pch=16);
+  points(v1[1], v1[2], col="yellow", lwd=2, pch=16);
   points(v2[1], v2[2], col="green", lwd=2, pch=16);
-  points(v3[1], v3[2], col="green", lwd=2, pch=16);
+  points(v3[1], v3[2], col="cyan", lwd=2, pch=16);
   legend("bottomright", 
          legend=c(
            "Base In", 
@@ -71,20 +91,161 @@ draw_points <- function(points, triangle, min_size, max_size)
            sprintf("(%.2g, %.2g)", v2[1], v2[2]), 
            sprintf("(%.2g, %.2g)", v3[1], v3[2])
           ), 
-         col=c("blue", "red", "green", "green", "green"), 
+         col=c("blue", "red", "yellow", "green", "cyan"), 
          pch=16
   );
-}
+  title(main=sprintf("Model n=%i f=%.5g", p_len, f));
+};
 
+generate_model <- function(min_size, max_size, triangle, n, f, plot_base=FALSE)
+{
+  points = generate_points(n, min_size, max_size);
+  points = classify_triangle(points, triangle);
+  points = generate_outliers(points, f);
+  
+  if (plot_base)
+  {
+    draw_points(points, triangle, min_size, max_size, f);
+  }
+  
+  return (points);
+};
 
+test_model <- function(min_size, max_size, triangle, n, f, k, test_size)
+{
+  points = generate_model(min_size, max_size, triangle, n, f);
+  
+  tests = generate_points(test_size, min_size, max_size);
+  tests = classify_triangle(tests, triangle);
+  
+  prediction = knn(points[,1:2], tests[,1:2], cl=factor(points[,3]), k=k);
+  prediction = prediction == 1;
+  
+  results = {};
+  
+  results["Error Count"] = sum(prediction != tests[,3]);
+  results["% Error Count"] = (results["Error Count"] / test_size) * 100.0;
+  
+  results["Correct Count"] = test_size - results["Error Count"];
+  results["% Correct"] = (results["Correct Count"] / test_size) * 100.0;
+  
+  results["False Positives Count"] = sum((!prediction) & tests[,3]);
+  results["% False Positives"] =
+    (results["False Positives Count"] / test_size) * 100.0;
+  
+  results["False Negative Count"] = sum(prediction & (!tests[,3]));
+  results["% False Negatives"] = 
+    (results["False Negative Count"] / test_size) * 100.0;
+  
+  return(results);
+};
+
+perform_tests <- function(min_size, max_size, triangle, n, f, k, test_size, repeats)
+{
+  cols = c(
+    "Error Count", "% Error Count", "Correct Count", 
+    "% Correct",  "False Positives Count", "% False Positives", 
+    "False Negatives Count", "% False Negatives Count");
+  results = data.frame(matrix(ncol = length(cols), nrow = repeats));
+  colnames(results) = cols;
+  
+  for(i in 1:repeats)
+  {
+    results[i,] =
+      test_model(min_size, max_size, triangle, n, f, k, test_size);
+  }
+  return(results);
+};
+
+default_repeat = 20
 default_min_size = 0
 default_max_size = 10
-
 default_triangle = create_triangle(c(3,3), c(7,3), c(7,7))
+default_n = 500;
+default_f = 0;
+default_k = 5;
+default_test_size = 10000;
 
-points = generate_points(1000, default_min_size, default_max_size)
+perform_first_question <- function(n_values)
+{
+  means_cols = c("Error Count Mean", "% Error Count Mean", "Correct Count Mean", 
+  "% Correct Mean",  "False Positives Count Mean", "% False Positives Count Mean", 
+  "False Negative Count Mean", "% False Negatives Count Mean");
+  sd_cols = c("Error Count SD", "% Error Count SD", "Correct Count SD", 
+    "% Correct SD",  "False Positives Count SD", "% False Positives SD", 
+    "False Negative Count SD", "% False Negatives Count SD");
+  cols = c(
+    "n", means_cols, sd_cols);
+  results = data.frame(matrix(ncol = length(cols), nrow = length(n_values)));
+  colnames(results) = cols;
+  
+  for(i in 1:length(n_values))
+  {
+    n = n_values[i];
+    result = perform_tests(
+      default_min_size, default_max_size, default_triangle, 
+      n, default_f, default_k, 
+      default_test_size, default_repeat);
+    results[i, "n"] = n;
+    results[i, means_cols] = sapply(result, mean);
+    results[i, sd_cols] = sapply(result, sd);
+    
+    png(sprintf("first/n_%i_boxplot.png", n));
+    boxplot(
+      result["Error Count"], 
+      ylab="Error Count", 
+      main=sprintf(
+        "n=%i mean=%g sd=%g", 
+        n, 
+        results[i, "Error Count Mean"],
+        results[i, "Error Count SD"]
+        )
+      );
+    dev.off();
+  }
+  write.csv(results, file = "first/data.csv")
+  png("first/error_percentage.png")
+  plot(
+    results$n, 
+    results$`% Error Count Mean`, 
+    type='l', col="red", xlab = "n", ylab = "Errors (%)",
+    ylim = c(
+      min(
+        results$`% Error Count Mean`, 
+        results$`% False Positives Count Mean`, 
+        results$`% False Negatives Count Mean`
+        ), 
+      max(
+        results$`% Error Count Mean`, 
+        results$`% False Positives Count Mean`, 
+        results$`% False Negatives Count Mean`
+        )
+      )
+    );
+  points(
+    results$n, 
+    results$`% False Positives Count Mean`, 
+    type='l', col="green");
+  points(
+    results$n, 
+    results$`% False Negatives Count Mean`, 
+    type='l', col="blue");
+  legend("topright", 
+         legend=c(
+           "Errors (%)", 
+           "False Positives (%)",
+           "False Negatives (%)"
+         ), 
+         col=c("red", "green", "blue"),
+         lty=1
+  );
+  dev.off();
+  return(results);
+};
 
-points = classify_triangle(points, default_triangle)
-draw_points(points, default_triangle, default_min_size, default_max_size)
+first_n_Values = c(100, 200, 300, 400, 500, 600, 700, 800);
+
+results = perform_first_question(first_n_Values);
+
 
 
